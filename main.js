8aa7416c16eeefdafad146b7adcc39408ee4bfdb
rukkysworld <136924362+rukkysworld@users.noreply.github.com>
2023-08-07 15:03:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var i = 5;
+var iisgreater = true;
+
+if (iisgreater = true) {
+    var i = 9;
+}
+
+
+if (i > 0) {
+    console.log(i)
+}
+
+console.warn("do not post any of your personal information in here!")
+
+document.createElement = user;
+
+function gotodefpage() {
+    document.write("Redirecting you to your page.... Just One Moment")
+    window.open("/about.html")
+}
+
+document.getElementById("buyorbought");
+
+function equipitem() {
+
+}
+
