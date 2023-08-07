9d23f33e655211e97cbc5bfb351929fe1933906e
rukkysworld <136924362+rukkysworld@users.noreply.github.com>
2023-08-07 16:51:03 -0400

    Add files via upload

---
+++ characterscripts/changepfp.js
@@ -0,0 +1,7 @@
+function changepfptoninjamato() {
+    document.getElementById('buyorbought').innerHTML = "Item is now EQUIPPED!";
+    document.getElementById('buyorbought').style.background = "green";
+    window.open('/index.html')
+    document.getElementById('').innerHTML = "<h1>Hello, World!</h1>"
+}
+
